5479db7938ef707e74a4f297ba7ac1b2f0b1516b
javaSlang <serhiylang@gmail.com>
2019-09-01 17:23:42 +0200

    VAT: output refactoring

--- src/main/java/com/slang/vat/service/impl/VatDataOutput.java
+++ src/main/java/com/slang/vat/service/impl/VatDataOutput.java
@@ -3,12 +3,23 @@ package com.slang.vat.service.impl;
 import com.slang.vat.domain.VatInfo;
 import com.slang.vat.service.DataOutput;
 import com.slang.vat.service.VatInfoData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.TreeSet;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
+import static java.text.MessageFormat.format;
+
 public class VatDataOutput implements DataOutput {
+
+    private static final Logger LOGGER = LogManager.getLogger(VatDataOutput.class);
+
+    private static final String RESULT_DIVIDING_LINE = "------------------- R E S U L T -------------------";
+    private static final String DIVIDING_LINE = "---------------------------------------------------";
+    private static final String OUTPUT_TEXT_PATTERN = " EU countries with the {0} standard VAT rate: ";
+    private static final String SERVICE_LINE_PATTERN = "{0}\n{1}{2}";
 
     private final TreeSet<VatInfo> vatInfos;
     private final int itemsToDisplay;
@@ -20,18 +31,19 @@ public class VatDataOutput implements DataOutput {
 
     @Override
     public void display() {
-        System.out.println("--------------------------------------------------- \n" +
-                "------------------- R E S U L T ------------------- \n" +
-                itemsToDisplay + " EU countries with the lowest standard VAT rate: ");
-        print(vatInfos::pollFirst);
-        System.out.println("--------------------------------------------------- \n" +
-                itemsToDisplay + " EU countries with the highest standard VAT rate: ");
-        print(vatInfos::pollLast);
-        System.out.println("---------------------------------------------------");
-        System.out.println("---------------------------------------------------");
+        LOGGER.debug("Displaying the results...");
+        printServiceLine(format(SERVICE_LINE_PATTERN, RESULT_DIVIDING_LINE, itemsToDisplay, format(OUTPUT_TEXT_PATTERN, "lowest")));
+        printSelectedResults(vatInfos::pollFirst);
+        printServiceLine(format(SERVICE_LINE_PATTERN, DIVIDING_LINE, itemsToDisplay, format(OUTPUT_TEXT_PATTERN, "highest")));
+        printSelectedResults(vatInfos::pollLast);
+        printServiceLine(DIVIDING_LINE);
     }
 
-    private void print(Supplier<VatInfo> selector) {
+    private void printServiceLine(String line) {
+        System.out.println(line);
+    }
+
+    private void printSelectedResults(Supplier<VatInfo> selector) {
         IntStream.rangeClosed(1, itemsToDisplay)
                 .mapToObj(i -> selector.get())
                 .forEachOrdered(System.out::println);
